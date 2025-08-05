96b65c6f1a45757aefaa074c758a8a50c41be2ce
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-05 22:54:43 +0300

    update contect

--- src/pages/about-us/components/Layout22.tsx
+++ src/pages/about-us/components/Layout22.tsx
@@ -1,35 +1,26 @@
 "use client";
 
-import { Button } from "@relume_io/relume-ui";
-import { RxChevronRight } from "react-icons/rx";
+import { ButtonLink } from "../../../components/ButtonLink";
 
 export function Layout22() {
   return (
     <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
-      <div className="container">
+      <div className="container lg:mx-auto">
         <div className="grid grid-cols-1 gap-y-12 md:grid-cols-2 md:items-center md:gap-x-12 lg:gap-x-20">
           <div>
             <h2 className="rb-5 mb-5 text-5xl font-bold md:mb-6 md:text-7xl lg:text-8xl">
-              Our Mission and Core Values
+              Our Journey &amp; Commitment to Sports
             </h2>
             <p className="md:text-md">
-              At 365Events, we believe in fostering a community where faith and
-              sports intersect. Our core values of discipline, teamwork, and
-              integrity guide us in making a positive impact on athletes and
-              their families.
+              Founded with a vision to transform local sports, 365Events has
+              been at the forefront of nurturing talent and fostering community
+              engagement. Our dedication to quality and innovation has made a
+              significant impact on the sports landscape in Kenya.
             </p>
             <div className="mt-6 flex flex-wrap items-center gap-4 md:mt-8">
-              <Button title="Learn More" variant="secondary">
-                Learn More
-              </Button>
-              <Button
-                title="Contact"
-                variant="link"
-                size="link"
-                iconRight={<RxChevronRight />}
-              >
+              <ButtonLink to="/contact" variant="chevron">
                 Contact
-              </Button>
+              </ButtonLink>
             </div>
           </div>
           <div>
