6486ef21737dab8b229793bb769ede96a2b98251
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-05 22:18:00 +0300

    update about hero

--- src/pages/about-us/components/Header47.tsx
+++ src/pages/about-us/components/Header47.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Button } from "@relume_io/relume-ui";
+import { ButtonLink } from "../../../components/ButtonLink";
 
 export function Header47() {
   return (
     <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
-      <div className="container">
+      <div className="container lg:mx-auto">
         <div className="flex flex-col gap-5 md:flex-row md:gap-12 lg:gap-20">
-          <div className="w-full max-w-lg">
+          <div className="w-full max-w-2xl">
             <p className="mb-3 font-semibold md:mb-4">Unity</p>
             <h1 className="text-6xl font-bold md:text-9xl lg:text-10xl">
               Faith in Sports
@@ -21,10 +21,9 @@ export function Header47() {
               growth.
             </p>
             <div className="mt-6 flex flex-wrap gap-4 md:mt-8">
-              <Button title="Learn More">Learn More</Button>
-              <Button title="Join Us" variant="secondary">
-                Join Us
-              </Button>
+              <ButtonLink to="/contact" variant="secondary">
+                Contact Us
+              </ButtonLink>
             </div>
           </div>
         </div>
