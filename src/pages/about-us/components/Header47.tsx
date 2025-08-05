77443824963695bcc4bf6df7884d15ad29c9f9d6
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-05 22:31:15 +0300

    update content

--- src/pages/about-us/components/Header47.tsx
+++ src/pages/about-us/components/Header47.tsx
@@ -4,25 +4,27 @@ import { ButtonLink } from "../../../components/ButtonLink";
 
 export function Header47() {
   return (
-    <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
+    <section
+      id="relume"
+      className="px-[5%] py-16 md:py-24 lg:py-28 bg-orange-100"
+    >
       <div className="container lg:mx-auto">
         <div className="flex flex-col gap-5 md:flex-row md:gap-12 lg:gap-20">
-          <div className="w-full max-w-2xl">
+          <div className="w-full max-w-3xl">
             <p className="mb-3 font-semibold md:mb-4">Unity</p>
             <h1 className="text-6xl font-bold md:text-9xl lg:text-10xl">
-              Faith in Sports
+              About 365events
             </h1>
           </div>
-          <div className="w-full max-w-lg">
+          <div className="w-full max-w-xl">
             <p className="md:text-md">
-              At 365Events, we believe in the transformative power of sports
-              through faith. Our mission is to inspire athletes to grow in
-              discipline, teamwork, and integrity while fostering spiritual
-              growth.
+              At 365Events, we believe in the transformative power of sports.
+              Our mission is to inspire athletes to grow in discipline,
+              teamwork, and integrity while fostering spiritual growth.
             </p>
             <div className="mt-6 flex flex-wrap gap-4 md:mt-8">
               <ButtonLink to="/contact" variant="secondary">
-                Contact Us
+                Join Us
               </ButtonLink>
             </div>
           </div>
