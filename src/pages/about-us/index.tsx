e3b27eb1a1eae1e087353cb393307e7dd61ed8b7
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-07 16:44:26 +0300

    update index

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,19 +1,22 @@
 import { Header47 } from "./components/Header47";
 import { Layout22 } from "./components/Layout22";
 import { Layout10 } from "./components/Layout10";
-import { Timeline13 } from "./components/Timeline13";
 import { Layout252 } from "./components/Layout252";
 import { Team16 } from "./components/Team16";
 import { Logo3 } from "./components/Logo3";
+import { Layout89 } from "./components/Layout89";
+import { Layout16 } from "./components/Layout16";
+import { Layout22_v1 } from "./components/Layout22_v1";
 
 export default function Page() {
   return (
     <div>
       <Header47 />
       <Layout22 />
-      <Layout10 />
-      <Timeline13 />
+      <Layout89 />
+      <Layout16 />
       <Layout252 />
+      <Layout22_v1 />
       <Team16 />
       <Logo3 />
     </div>
