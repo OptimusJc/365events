2ab95369c04559ac92abed3ff8f51f88f1566dd6
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-07 15:47:57 +0300

    add new section Layout22_v1

---
+++ src/pages/about-us/components/Layout22_v1.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+export function Layout22_v1() {
+  return (
+    <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
+      <div className="container lg:mx-auto">
+        <div className="grid grid-cols-1 gap-y-12 md:grid-cols-2 md:items-center md:gap-x-12 lg:gap-x-20">
+          <div>
+            <h2 className="rb-5 mb-5 text-5xl font-bold md:mb-6 md:text-7xl lg:text-8xl">
+              Our Values: Inspiring Discipline, Purpose, and Service in Sports
+              and Community.
+            </h2>
+            <p className="md:text-md">
+              At 365Events, we believe that sports can be a powerful tool for
+              personal growth and community development. Our commitment to
+              discipline, purpose, and service guides our mission to empower
+              young athletes and foster a supportive environment.
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
+              className="w-full rounded-image object-cover"
+              alt="Relume placeholder image"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
