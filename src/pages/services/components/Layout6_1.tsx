72d77b74070d7721911919a732c7fefd0ffd39ed
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-13 16:53:39 +0300

    update background color

--- src/pages/services/components/Layout6_1.tsx
+++ src/pages/services/components/Layout6_1.tsx
@@ -2,8 +2,11 @@
 
 export function Layout6_1() {
   return (
-    <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
-      <div className="container">
+    <section
+      id="relume"
+      className="px-[5%] py-16 md:py-24 lg:py-28 bg-orange-50"
+    >
+      <div className="container mx-auto">
         <div className="grid grid-cols-1 gap-y-12 md:grid-flow-row md:grid-cols-2 md:items-center md:gap-x-12 lg:gap-x-20">
           <div>
             <h1 className="rb-5 mb-5 text-4xl leading-[1.2] font-bold md:mb-6 md:text-5xl lg:text-6xl">
