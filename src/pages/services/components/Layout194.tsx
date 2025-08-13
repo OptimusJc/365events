0e257f1c15a1e132673af19cd7a74bc5f2ac531b
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-13 16:52:37 +0300

    update layout and background color

--- src/pages/services/components/Layout194.tsx
+++ src/pages/services/components/Layout194.tsx
@@ -2,8 +2,11 @@
 
 export function Layout194() {
   return (
-    <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
-      <div className="container">
+    <section
+      id="relume"
+      className="px-[5%] py-16 md:py-24 lg:py-28 bg-orange-100"
+    >
+      <div className="container mx-auto">
         <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 lg:gap-x-20">
           <div className="order-2 md:order-1">
             <img
