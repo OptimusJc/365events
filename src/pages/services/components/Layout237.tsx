a08f476af97b11ef4918bb203a1a51719f6ec3a8
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-13 17:34:02 +0300

    add mx-auto

--- src/pages/services/components/Layout237.tsx
+++ src/pages/services/components/Layout237.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { Button } from "@relume_io/relume-ui";
-import { RxChevronRight } from "react-icons/rx";
+import { TbTournament } from "react-icons/tb";
+import { MdAllInclusive, MdOutlineSports } from "react-icons/md";
+import { ButtonLink } from "../../../components/ButtonLink";
 
 export function Layout237() {
   return (
     <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
-      <div className="container">
+      <div className="container mx-auto">
         <div className="flex flex-col items-center">
-          <div className="rb-12 mb-12 w-full max-w-lg text-center md:mb-18 lg:mb-20">
+          <div className="rb-12 mb-12 w-full max-w-2xl text-center md:mb-18 lg:mb-20">
             <p className="mb-3 font-semibold md:mb-4">Events</p>
             <h2 className="rb-5 mb-5 text-5xl font-bold md:mb-6 md:text-7xl lg:text-8xl">
               Exciting Sports Events for Everyone
@@ -23,11 +24,7 @@ export function Layout237() {
           <div className="grid grid-cols-1 items-start justify-center gap-y-12 md:grid-cols-3 md:gap-x-8 md:gap-y-16 lg:gap-x-12">
             <div className="flex w-full flex-col items-center text-center">
               <div className="rb-5 mb-5 md:mb-6">
-                <img
-                  src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
-                  alt="Relume logo 1"
-                  className="size-12"
-                />
+                <TbTournament className="h-12 w-12" />
               </div>
               <h3 className="mb-5 text-2xl font-bold md:mb-6 md:text-3xl md:leading-[1.3] lg:text-4xl">
                 Tournaments and Competitive Matches
@@ -38,11 +35,7 @@ export function Layout237() {
             </div>
             <div className="flex w-full flex-col items-center text-center">
               <div className="rb-5 mb-5 md:mb-6">
-                <img
-                  src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
-                  alt="Relume logo 1"
-                  className="size-12"
-                />
+                <MdOutlineSports className="h-12 w-12" />
               </div>
               <h3 className="mb-5 text-2xl font-bold md:mb-6 md:text-3xl md:leading-[1.3] lg:text-4xl">
                 Community Sports Days for All Ages
@@ -51,11 +44,7 @@ export function Layout237() {
             </div>
             <div className="flex w-full flex-col items-center text-center">
               <div className="rb-5 mb-5 md:mb-6">
-                <img
-                  src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
-                  alt="Relume logo 1"
-                  className="size-12"
-                />
+                <MdAllInclusive className="h-12 w-12" />
               </div>
               <h3 className="mb-5 text-2xl font-bold md:mb-6 md:text-3xl md:leading-[1.3] lg:text-4xl">
                 Inclusive Matches for All Skill Levels
@@ -64,10 +53,12 @@ export function Layout237() {
             </div>
           </div>
           <div className="mt-10 flex items-center gap-4 md:mt-14 lg:mt-16">
-            <Button variant="secondary">Learn More</Button>
-            <Button iconRight={<RxChevronRight />} variant="link" size="link">
+            <ButtonLink to="/about" variant="secondary">
+              Lean More
+            </ButtonLink>
+            <ButtonLink to="/contact" variant="chevron">
               Sign Up
-            </Button>
+            </ButtonLink>
           </div>
         </div>
       </div>
